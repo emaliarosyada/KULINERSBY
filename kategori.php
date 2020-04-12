e0adb558a255ba88f56b99932288923ded788591
Emalia Rosyada <emaliarosyada@gmail.com>
2020-04-12 14:31:28 +0700

    Pembuatan halaman kategori

---
+++ kategori.php
@@ -0,0 +1,158 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <!-- My Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Viga" rel="stylesheet">
+
+    <!-- My CSS -->
+    <link rel="stylesheet" href="style.css">
+
+    <title> Kategori </title>
+  </head>
+  <body>
+    
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light ">
+      <div class="container">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav ml-auto">
+            <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link" href="#">Pricing</a>
+            <a class="nav-item nav-link" href="#">Features</a>
+            <a class="nav-item nav-link" href="#">About</a>
+            <a class="nav-item btn btn-primary tombol" href="#">Join us</a>
+          </div>
+        </div>
+       </div>
+    </nav>
+    <!-- Akhir navbar -->
+
+    <!-- Jumbotron -->
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Get work done <span>faster</span> <br>   and <span>better</span> with us</h1>
+         <a href="" class="btn btn-primary" > Our Work</a>
+          
+      </div>
+    </div 
+    <!-- Akhir Jumbotron -->
+
+    <!-- Container -->
+    <div class="container">
+
+      <!-- info panel-->
+      <div class="row justify-content-center">
+        <div class="col-lg-10 info-panel">
+          <div class="row">
+            <div class="col-lg">
+              <img src="img/employee.png" alt="Employee" class="float-left">
+              <h4> 24 Hours</h4>
+              <p> Lorem ipsum dolor sit amet</p>
+            </div>
+            <div class="col-lg">
+              <img src="img/hires.png" alt="Hires" class="float-left">
+              <h4> 24 Hours</h4>
+              <p> Lorem ipsum dolor sit amet</p>
+            </div>
+            <div class="col-lg">
+              <img src="img/security.png" alt="Security" class="float-left">
+              <h4> 24 Hours</h4>
+              <p> Lorem ipsum dolor sit amet</p>
+            </div>
+            </div>
+          </div>
+
+        </div>
+        
+      </div>
+
+      <!-- akhir info panel -->
+
+        <!-- Workingspace -->
+        <div class="row workingspace ">
+          <div class="col-lg-6 ">
+            <img src="img/workingspace.png" alt="workingspace" class="img-fluid">
+          </div>
+          <div class="col-lg-5">
+            <h3>You <span>work</span> like at <span> home</span></h3>
+            <p> Bekerja dengan suasana hati yang lebih asik dan mempelajari hal baru setiap harinya</p>
+            <a href="" class="btn btn-primary tombol">Gallery</a>  
+          </div>
+        </div>
+        <!-- akhir workingspace -->
+
+        <!-- Testimonial -->
+        <section class="testimonial">
+          <div class="row justify-content-center">
+            <div class="col-lg-8">
+              <h5>"Bekerja dengan suasana hati yang lebih asik dan mempelajari hal baru setiap harinya"</h5>
+            </div>
+          </div>
+
+          <div class="row justify-content-center">
+            <div class="col-6 justify-content-center d-flex">
+              <figure class="figure">
+                <img src="img/img2.png"  class="figure-img img-fluid rounded-circle" alt="Testi 1">
+                <figcaption class="figure-caption">
+                  <h5>Sunny Ye</h5>
+                  <p>Designer</p>
+                </figcaption>
+              </figure>
+            </div>
+          </div>
+        </section>
+
+<!-- footer -->
+<div class="row footer">
+<div class="col text-center">
+  <p>2018 All Rights Reserved by Rosa</p>
+</div>
+</div>
+    </div>
+    <!-- akhir container -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min .js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"  crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
