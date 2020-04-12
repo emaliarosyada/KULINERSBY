e098f4fe2862987f255679a188010c085faab9ad
Surendroa22 <54930935+Surendroa22@users.noreply.github.com>
2020-04-13 00:12:47 +0700

    restaurant
    membuat info tentang restaurant

---
+++ restoran1.php
@@ -0,0 +1,55 @@
+<html>
+<head>
+<style>
+
+    b {
+        color: blue
+    }
+</style>
+<title>Tempat makan di Surabaya</title>
+<link rel="stylesheet" type="text/CSS" href="First.css">
+</head>
+<body>
+    <h2 span style="color: coral"><center>Soto Lamongan Cak Har</h2></center>
+    <center><img src="https://media-cdn.tripadvisor.com/media/photo-s/06/b7/66/fc/chicken-soup-with-indonesian.jpg"width="300" height="250"></center>
+    
+    <p><left> </p>
+    <b id="restaurant"><center>Soto Lamongan Cak Har</center></b>
+    <b id="rating"><center>Rating : 5 </center></b>
+
+    <p><left>info</left></p>
+    <span class="left">
+                  
+        Jl. Arief Rahman Hakim No. 136, Sukolilo, Surabaya
+      </span>
+      <span class="clearfix"></span>
+    </p>
+      <p class="large-screen-toggle">
+        <i class="fa fa-clock-o left"></i>
+        <span class="left">
+            
+            <span>Senin - Minggu (06:00 - 22:00)</span>
+              <i class="fa fa-circle sm-dot"></i>
+              <i class="fa fa-phone"></i>
+          
+        <span class="clearfix"></span>
+      </p>
+      <p class="small-screen-toggle">
+        <i class="fa fa-phone left"></i>
+        <span class="left" itemprop="telephone">
+          <a href="tel:085704265904">085704265904</a>
+        </span>
+        <span class="clearfix"></span>
+      </p>
+      
+    <p>
+      <i class="icon icon-price left"></i>
+      <span class="left" itemprop="priceRange" id="avg-price">
+        Di bawah Rp. 50.000 /orang
+      </span>
+      <span class="clearfix"></span>
+    </p>
+        
+    
+</body>
+</html>
